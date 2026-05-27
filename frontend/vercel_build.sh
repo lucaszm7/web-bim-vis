0de5ebb8ff09d577bbfbe54f94bdc55806c7c3ef
Lucas Morais <lucas.morais446@hotmail.com>
2026-05-27 00:27:58 -0300

    wip

--- frontend/vercel_build.sh
+++ frontend/vercel_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # 1. Install wasm-pack
-echo "Installing wasm-pack..."
+echo "Installing wasm-pack on vercel..."
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 # 2. Build your Rust project into WebAssembly
 # (Replace './your-rust-folder' with the actual path to your Rust code)
-echo "Compiling Rust to WASM..."
+echo "Compiling Rust to WASM on vercel..."
 wasm-pack build ./your-rust-folder --target web
 
 # 3. Run your standard frontend build 
